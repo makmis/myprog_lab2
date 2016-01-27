3cf88611f1f0256ff3acda26ffe7d12e17a48f49
makmis <mixmax13@mail.ru>
2016-01-27 11:43:00 +0300

    создал директроию pwdx, начал разработку pwdx.c

---
+++ pwdx/pwdx.c
@@ -0,0 +1,17 @@
+/*pwdx.c*/
+	//подключаем заголовочные файлы, библиотеку
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+int main(int argc, char* argv[])
+	//добовляем функцию
+{
+
+printf("%s\n", buff);
+	//выводим строку на экран
+return 0;
+
+
+}
