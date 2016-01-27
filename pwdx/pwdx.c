3447a98e672c882c88f10388d3af57d78f0a70cd
makmis <mixmax13@mail.ru>
2016-01-27 11:55:40 +0300

    добавил изменения в файл

--- pwdx/pwdx.c
+++ pwdx/pwdx.c
@@ -8,6 +8,10 @@
 int main(int argc, char* argv[])
 	//добовляем функцию
 {
+        //выделяем буфер для вывода
+char buff[256];
+char* gde = argv[1];
+
 
 printf("%s\n", buff);
 	//выводим строку на экран
